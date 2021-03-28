9c3b291605f8134deebc24766f7fd5bf1d9edb8f
melody2005 <55598373+melody2005@users.noreply.github.com>
2021-03-28 00:17:38 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+game.splash("Today we will calculate the area of a rectangle")
+let Length = game.askForNumber("Enter the length in cm:")
+let Width = game.askForNumber("Enter the width in cm:")
+let Area = Length * Width
+game.splash("The area of the rectangle with length " + Length + "cm and width " + Width + "cm is " + Area + "cm squared.")
+let Perimeter = 2 * Length + 2 * Width
+game.splash("The area of the rectangle with length " + Length + "cm and width " + Width + "cm is " + Area + "cm squared.")
